f0e92abf3cb8895bfbe291809b28e68798712d4e
jamesburgess44 <jamesburgess44@yahoo.com>
2021-05-28 13:15:00 -0600

    added alert that displays a message to the user shwoing details of trip and then gets logged to console.

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -6,7 +6,7 @@ let entertainment = ["Sports Event","Hiking","Zoo","Museum"]
 
 function pickRandomDestination(){
     let randomDestination = destinations[Math.floor(Math.random()*destinations.length)];
-    console.log(randomDestination);
+    //console.log(randomDestination);
     return randomDestination;
 }
 pickRandomDestination();
@@ -14,14 +14,14 @@ pickRandomDestination();
 
 function pickRandomRestaurant(){
     let randomRestaurants = restaurants[Math.floor(Math.random()*restaurants.length)];
-    console.log(randomRestaurants);
+    //console.log(randomRestaurants);
     return randomRestaurants;
 }
 pickRandomRestaurant();
 
 function pickRandomTransportation(){
     let randomTransportation = transportation[Math.floor(Math.random()*transportation.length)];
-    console.log(randomTransportation);
+    //console.log(randomTransportation);
     return randomTransportation;
 }
 pickRandomTransportation();
@@ -29,7 +29,21 @@ pickRandomTransportation();
 
 function pickRandomEntertainment(){
     let randomEntertainment = entertainment[Math.floor(Math.random()*entertainment.length)];
-    console.log(randomEntertainment);
+    //console.log(randomEntertainment);
     return randomEntertainment;
 } 
 pickRandomEntertainment()
+
+
+
+let dayTripArray = [pickRandomDestination(), pickRandomRestaurant(), pickRandomTransportation(), pickRandomEntertainment()];
+let userDestination = dayTripArray[0];
+let userRestaurant = dayTripArray[1];
+let userTransporation = dayTripArray[2];
+let userEntertainment = dayTripArray[3];
+
+function userTrip(){
+    alert("Your randomly generated day trip will be in " + userDestination + ", you'll eat at " + userRestaurant + ", you'll arrive by " + userTransporation + ", and you will go to a " + userEntertainment + ".");
+    console.log("Your randomly generated day trip will be in " + userDestination + ", you'll eat at " + userRestaurant + ", you'll arrive by " + userTransporation + ", and you will go to a " + userEntertainment + ".")
+}
+userTrip();
